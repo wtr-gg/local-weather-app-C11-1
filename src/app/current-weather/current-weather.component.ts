3ee1534b5e3d081e157d124d28013c03091240d0
Kal <info@kalacademy.org>
2021-10-03 13:27:23 -0700

    Completed app

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
-import { WeatherService } from '../weather.service';
-
 
 @Component({
   selector: 'app-current-weather',
@@ -9,9 +7,9 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
-  current: ICurrentWeather
-  
-  constructor(private weatherService: WeatherService) {
+  @Input() current: ICurrentWeather
+
+  constructor() {
     this.current = {
       city: '',
       country: '',
@@ -22,8 +20,6 @@ export class CurrentWeatherComponent implements OnInit {
     }
    }
 
-  ngOnInit(): void {
-    this.weatherService.getCurrentWeather('Redmond', 'US').subscribe(data => this.current = data )
-  }
+  ngOnInit(): void { }
 
 }
