fe85153ccd6fa7c989cccaa75b24eec6550dae12
wtr-gg <78711816+wtr-gg@users.noreply.github.com>
2021-10-08 03:32:38 -0700

    added second component

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -16,14 +16,16 @@ export class WeatherService {
     let uriParams = '';
 
     if (typeof search === 'string'){
-      uriParams = `q=${search}`;
-    } else {
-      uriParams = `zip=${search}`;
+      uriParams = `q=${search}`
+    } else { 
+      uriParams = `zip=${search}`
     }
 
     if(country){
-      uriParams = `${uriParams},${country}`;
+      uriParams = `${uriParams},${country}`
     }
+
+    
 
      return this.httpClient.get<ICurrentWeatherData>(`https://api.openweathermap.org/data/2.5/weather?${uriParams}&appid=${environment.appId}`).pipe(
       map(data => this.transformToICurrentWeather(data))
